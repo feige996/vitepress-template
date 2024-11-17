457c040c1e439e41b98280abfdfc52d978f182ab
Burt <burt.lai@kupotech.com>
2024-11-17 16:30:47 +0800

    feat: 版本号

---
+++ docs/.vitepress/theme/components/NavBarTitleAfter.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+/**
+ * NavBarTitleAfter 组件用于显示当前项目的版本号。
+ * 版本号从 package.json 文件中读取，并以绿色小字体显示。
+ */
+import packageConfig from '../../../../package.json'
+</script>
+
+<template>
+  <span
+    style="
+      color: green;
+      font-size: 10px;
+      margin-left: 4px;
+      background-color: azure;
+      padding: 0 4px;
+      border-radius: 4px;
+      display: inline-block;
+      font-weight: 700;
+    "
+    >{{ packageConfig.version }}</span
+  >
+</template>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,7 +1,9 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
+import { h } from 'vue'
+/** NavBarTitleAfter 组件用于项目版本号 */
+import NavBarTitleAfter from './components/NavBarTitleAfter.vue'
 import './style.css'
 
 export default {
@@ -9,9 +11,10 @@ export default {
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
+      'nav-bar-title-after': () => h(NavBarTitleAfter),
     })
   },
   enhanceApp({ app, router, siteData }) {
     // ...
-  }
+  },
 } satisfies Theme
